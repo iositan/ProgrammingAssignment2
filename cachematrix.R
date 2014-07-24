e1043e8011ad9c40083f8dc44dbbed56f01c0988
Iosif Tanasescu <iosif.tanasescu@iaaiqc.com>
2014-07-23 21:18:31 -0400

    Pseudo initial commit. Minimal changes and RStudio project added THe .gitignore is added only to keep track of what will be ignored (we intend no file to be ignored)

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This is the "class" definition in R style
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This one is another class
+# it will instantiate a class of type makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
