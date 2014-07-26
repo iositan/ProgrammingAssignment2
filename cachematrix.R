3a039c6e03529e99a0e7c352cce0dfef47e3d5ef
Iosif Tanasescu <iosif.tanasescu@iaaiqc.com>
2014-07-26 18:47:10 -0400

    First iteration

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Description
+# Matrix inversion is usually a costly computation and there may 
+# be some benefit to caching the inverse of a matrix rather than
+# computing it repeatedly (there are also alternatives to matrix 
+# inversion that we will not discuss here). 
+
+# Those are a pair of functions that cache the inverse of a matrix.
 
 ## This is the "class" definition in R style
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # mat_cache is local variable which store the cached matrix (if any)
+    mat_cache <- NULL 
+    # OOP getter/setter
+    # setter will initialize the  matrix and reset the mat_cache
+    set <- function(y) {
+        # these  are from another environment !
+        x <<- y
+        mat_cache <<- NULL
+    }
+    # getter is a simple one line function
+    get <- function() x
+    
+    # here comes the special methods
+    setinverse <- function(inverse) mat_cache <<- inverse
+    #
+    getinverse <- function() mat_cache
+    
+    # return an object of type list
+    list(set = set, get = get,
+         setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -13,4 +37,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    # note that mat_cache is local here
+    mat_cache <- x$getinverse()
+    if (!is.null(mat_cache)) {
+        message("getting cached data")
+        return(mat_cache)
+    }
+    
+    matrix_data <- x$get()
+    mat_cache <- solve(matrix_data,...)
+    x$setmean(mat_cache)
+    mat_cache
+    # the above was the return
 }
